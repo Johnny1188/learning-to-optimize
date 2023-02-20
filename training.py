a55d82cc6afd3f3e46385ac3d4ea211993739b4d
Jan Sobotka <jsobotka1188@gmail.com>
2023-02-20 17:05:09 +0100

    add regularization loss and additional metrics to training

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import copy
+import json
 import os
 
 import numpy as np
@@ -6,7 +7,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 from meta_module import *
 from optimizer import Optimizer
@@ -16,13 +17,17 @@ from utils.others import count_parameters, detach_var, rgetattr, rsetattr, w
 def do_fit(
     opter,
     opter_optim,
-    target_cls,
+    data_cls,
     optee_cls,
     unroll,
     n_iters,
     optee_updates_lr,
     train_opter=True,
+    log_unroll_losses=False,
+    opter_updates_reg_func=None,
+    reg_mul=1.0,
     optee_config=None,
+    eval_iter_freq=10,
     ckpt_iter_freq=None,
     ckpt_prefix="",
     ckpt_dir="",
@@ -34,7 +39,8 @@ def do_fit(
         opter.eval()
         unroll = 1
 
-    target = target_cls(training=train_opter)
+    train_data = data_cls(training=True)
+    test_data = data_cls(training=False)
     optee = w(optee_cls(**optee_config if optee_config is not None else {}))
     optee_n_params = sum(
         [int(np.prod(p.size())) for _, p in optee.all_named_parameters()]
@@ -48,18 +54,26 @@ def do_fit(
         w(Variable(torch.zeros(optee_n_params, opter.hidden_sz))) for _ in range(2)
     ]
 
-    all_losses = []
+    metrics = {m: [] for m in ["train_loss", "train_acc", "test_loss", "test_acc"]}
     unroll_losses = None
+    reg_losses = None
     updates_for_ckpt = dict()
-    for iteration in range(1, n_iters + 1):
-        loss = optee(target)
 
-        unroll_losses = loss if unroll_losses is None else unroll_losses + loss
-        all_losses.append(loss.data.cpu().numpy())
-        loss.backward(retain_graph=train_opter)
+    ### run optee's training loop
+    for iteration in range(1, n_iters + 1):
+        ### train optee
+        optee.train()
+        train_loss, train_acc = optee(train_data, return_acc=True)  # a single minibatch
+        train_loss.backward(retain_graph=train_opter)
+
+        ### track for training opter
+        unroll_losses = (
+            train_loss if unroll_losses is None else unroll_losses + train_loss
+        )
 
         ### optimizer: gradients -> updates
-        result_params = {}
+        result_params = dict()
+        updates_for_reg = dict()
         hidden_states2 = [
             w(Variable(torch.zeros(optee_n_params, opter.hidden_sz))) for _ in range(2)
         ]
@@ -71,24 +85,62 @@ def do_fit(
             if p.requires_grad == False:  # batchnorm stats
                 result_params[name] = p
                 continue
-            cur_sz = int(np.prod(p.size()))
+
             # We do this so the gradients are disconnected from the graph but we still get
             # gradients from the rest
+            cur_sz = int(np.prod(p.size()))
             gradients = detach_var(p.grad.view(cur_sz, 1))
             updates, new_hidden, new_cell = opter(
-                gradients,
-                [h[offset : offset + cur_sz] for h in hidden_states],
-                [c[offset : offset + cur_sz] for c in cell_states],
+                optee_grads=gradients,
+                hidden=[h[offset : offset + cur_sz] for h in hidden_states],
+                cell=[c[offset : offset + cur_sz] for c in cell_states],
+                additional_inp=None
             )
+
+            ### track updates for checkpointing
             if ckpt_iter_freq and iteration % ckpt_iter_freq == 0:
                 updates_for_ckpt[name] = updates.view(*p.size()).detach()
+
+            ### track updates for regularization
+            if train_opter and opter_updates_reg_func is not None:
+                updates_for_reg[name] = updates.view(*p.size())
+
+            ### update hidden and cell states
             for i in range(len(new_hidden)):
                 hidden_states2[i][offset : offset + cur_sz] = new_hidden[i]
                 cell_states2[i][offset : offset + cur_sz] = new_cell[i]
+
+            ### update optee's params
             result_params[name] = p + optee_updates_lr * updates.view(*p.size())
             result_params[name].retain_grad()
-
             offset += cur_sz
+
+        ### add regularization loss for opter
+        if train_opter and opter_updates_reg_func is not None:
+            reg_loss = torch.abs(
+                reg_mul
+                * opter_updates_reg_func(
+                    updates=updates_for_reg, optee=optee, lr=optee_updates_lr
+                )
+            )
+            reg_losses = reg_loss if reg_losses is None else reg_losses + reg_loss
+            updates_for_reg = dict()
+            # add to metrics
+            if "train_reg_loss" not in metrics:
+                metrics["train_reg_loss"] = []
+            metrics["train_reg_loss"].append(reg_loss.item())
+
+        ### track metrics
+        metrics["train_loss"].append(train_loss.item())
+        metrics["train_acc"].append(train_acc.item())
+
+        ### eval
+        if eval_iter_freq is not None and iteration % eval_iter_freq == 0:
+            optee.eval()
+            test_loss, test_acc = optee(test_data, return_acc=True)
+            metrics["test_loss"].append(test_loss.item())
+            metrics["test_acc"].append(test_acc.item())
+            optee.train()
 
         ### checkpoint
         if ckpt_iter_freq and iteration % ckpt_iter_freq == 0:
@@ -97,49 +149,62 @@ def do_fit(
                 "optimizee_grads": {k: v.grad for k, v in optee.all_named_parameters()},
                 "optimizee_updates": updates_for_ckpt,
                 "optimizer": opter.state_dict(),
-                "loss_history": all_losses,
+                "metrics": metrics,
             }
             torch.save(ckpt, os.path.join(ckpt_dir, f"{ckpt_prefix}{iteration}.pt"))
             updates_for_ckpt = dict()
 
         ### update - continue unrolling or step w/ opter
         if iteration % unroll == 0:
-            # step w/ the optimizer
+            ### step w/ the optimizer
             if train_opter:
                 opter_optim.zero_grad()
-                unroll_losses.backward()
+                if log_unroll_losses:
+                    unroll_losses = torch.log(unroll_losses)
+                total_loss = (
+                    unroll_losses + reg_losses
+                    if reg_losses is not None
+                    else unroll_losses
+                )
+                total_loss.backward()
                 opter_optim.step()
 
-            # reinitialize - start next unroll
+            ### reinitialize - start next unroll
             optee = w(optee_cls(**optee_config if optee_config is not None else {}))
             optee.load_state_dict(result_params)
             optee.zero_grad()
             hidden_states = [detach_var(v) for v in hidden_states2]
             cell_states = [detach_var(v) for v in cell_states2]
             unroll_losses = None
+            reg_losses = None
         else:
-            # update the optimizee and optimizer's states
+            ### update the optimizee and optimizer's states
             for name, p in optee.all_named_parameters():
                 if p.requires_grad:  # batchnorm stats
                     rsetattr(optee, name, result_params[name])
             hidden_states = hidden_states2
             cell_states = cell_states2
 
-    return all_losses
+    return metrics
 
 
 def fit_optimizer(
-    target_cls,
+    data_cls,
     optee_cls,
     optee_config=None,
-    preproc=False,
+    opter_cls=Optimizer,
+    opter_config=None,
     unroll=20,
     n_epochs=20,
     n_optim_runs_per_epoch=20,
     n_iters=100,
     n_tests=100,
-    opter_lr=0.001,
+    opter_lr=0.01,
+    log_unroll_losses=False,
+    opter_updates_reg_func=None,
+    reg_mul=1.0,
     optee_updates_lr=1.0,
+    eval_iter_freq=10,
     ckpt_iter_freq=None,
     ckpt_prefix="",
     ckpt_dir="",
@@ -147,72 +212,113 @@ def fit_optimizer(
     if ckpt_iter_freq is not None:
         os.makedirs(ckpt_dir, exist_ok=True)
 
-    opt_net = w(Optimizer(preproc=preproc))
-    meta_opt = optim.Adam(opt_net.parameters(), lr=opter_lr)
+    opter = w(opter_cls(**opter_config if opter_config is not None else {}))
+    meta_opt = optim.Adam(opter.parameters(), lr=opter_lr)
 
-    best_net = None
+    best_opter = None
     best_loss = np.inf
+    all_metrics = list()
 
     for epoch_i in tqdm(range(n_epochs), "epochs"):
-        ### train
-        train_loss = 0.0
+        all_metrics.append({k: dict() for k in ["meta_training", "meta_testing"]})
+
+        ### meta-train
         for _ in tqdm(range(n_optim_runs_per_epoch), "optimization runs"):
-            train_loss += np.sum(
-                do_fit(
-                    opter=opt_net,
-                    opter_optim=meta_opt,
-                    target_cls=target_cls,
-                    optee_cls=optee_cls,
-                    optee_config=optee_config,
-                    unroll=unroll,
-                    n_iters=n_iters,
-                    optee_updates_lr=optee_updates_lr,
-                    train_opter=True,
-                    ckpt_iter_freq=ckpt_iter_freq,
-                    ckpt_prefix=f"{ckpt_prefix}{epoch_i}e_",
-                    ckpt_dir=ckpt_dir,
-                )
+            optim_run_metrics = do_fit(
+                opter=opter,
+                opter_optim=meta_opt,
+                data_cls=data_cls,
+                optee_cls=optee_cls,
+                optee_config=optee_config,
+                unroll=unroll,
+                n_iters=n_iters,
+                optee_updates_lr=optee_updates_lr,
+                train_opter=True,
+                log_unroll_losses=log_unroll_losses,
+                opter_updates_reg_func=opter_updates_reg_func,
+                reg_mul=reg_mul,
+                eval_iter_freq=eval_iter_freq,
+                ckpt_iter_freq=ckpt_iter_freq,
+                ckpt_prefix=f"{ckpt_prefix}{epoch_i}e_",
+                ckpt_dir=ckpt_dir,
             )
-        train_loss /= n_optim_runs_per_epoch
-        print(f"[{epoch_i + 1}/{n_epochs}] Training loss: {train_loss}")
+            for k, v in optim_run_metrics.items():
+                if k not in all_metrics[epoch_i]["meta_training"]:
+                    all_metrics[epoch_i]["meta_training"][k] = np.array(v)
+                else:
+                    all_metrics[epoch_i]["meta_training"][k] += np.array(v)
 
-        ### test
-        test_loss = 0.0
+        ### average metrics and log
+        for k, v in all_metrics[epoch_i]["meta_training"].items():
+            v = v / n_optim_runs_per_epoch
+            all_metrics[epoch_i]["meta_training"][k] = {
+                "sum": np.sum(v),
+                "last": v[-1]
+                if (type(v) in (list, np.ndarray) and len(v) > 0)
+                else np.nan,
+            }
+
+        print(
+            f"[{epoch_i + 1}/{n_epochs}] Meta-training metrics:"
+            f"\n{json.dumps(all_metrics[epoch_i]['meta_training'], indent=4, sort_keys=False)}"
+        )
+
+        ### meta-test
         for _ in tqdm(range(n_tests), "tests"):
-            test_loss += np.sum(
-                do_fit(
-                    opter=opt_net,
-                    opter_optim=meta_opt,
-                    target_cls=target_cls,
-                    optee_cls=optee_cls,
-                    optee_config=optee_config,
-                    unroll=unroll,
-                    n_iters=n_iters,
-                    optee_updates_lr=optee_updates_lr,
-                    train_opter=False,
-                    ckpt_iter_freq=None,
-                )
+            optim_run_metrics = do_fit(
+                opter=opter,
+                opter_optim=meta_opt,
+                data_cls=data_cls,
+                optee_cls=optee_cls,
+                optee_config=optee_config,
+                unroll=unroll,
+                n_iters=n_iters,
+                optee_updates_lr=optee_updates_lr,
+                train_opter=False,
+                eval_iter_freq=eval_iter_freq,
+                ckpt_iter_freq=None,
             )
-        test_loss /= n_tests
-        print(f"[{epoch_i + 1}/{n_epochs}] Testing loss: {test_loss}")
+            for k, v in optim_run_metrics.items():
+                if k not in all_metrics[epoch_i]["meta_testing"]:
+                    all_metrics[epoch_i]["meta_testing"][k] = np.sum(v)
+                else:
+                    all_metrics[epoch_i]["meta_testing"][k] += np.sum(v)
 
-        if test_loss < best_loss:
+        ### average metrics and log
+        for k, v in all_metrics[epoch_i]["meta_testing"].items():
+            v = v / n_tests
+            all_metrics[epoch_i]["meta_testing"][k] = {
+                "sum": np.sum(v),
+                "last": v[-1]
+                if (type(v) in (list, np.ndarray) and len(v) > 0)
+                else np.nan,
+            }
+
+        print(
+            f"[{epoch_i + 1}/{n_epochs}] Meta-testing metrics:"
+            f"\n{json.dumps(all_metrics[epoch_i]['meta_testing'], indent=4, sort_keys=False)}"
+        )
+
+        if all_metrics[epoch_i]["meta_testing"]["train_loss"]["sum"] < best_loss:
             print(
-                f"[{epoch_i + 1}/{n_epochs}] New best loss\n\t previous: \t{best_loss}\n\t current:\t {test_loss}"
+                f"[{epoch_i + 1}/{n_epochs}] New best training loss"
+                f"\n\t previous:\t {best_loss}"
+                f"\n\t current:\t {all_metrics[epoch_i]['meta_testing']['train_loss']['sum']}"
             )
-            best_loss = test_loss
-            best_net = copy.deepcopy(opt_net.state_dict())
+            best_loss = all_metrics[epoch_i]["meta_testing"]["train_loss"]["sum"]
+            best_opter = copy.deepcopy(opter.state_dict())
 
-    return best_loss, best_net
+    return best_loss, all_metrics, best_opter
 
 
 def fit_normal(
-    target_cls,
+    data_cls,
     optee_cls,
     opter_cls,
     n_tests=100,
     n_iters=100,
     optee_config=None,
+    eval_iter_freq=10,
     ckpt_iter_freq=None,
     ckpt_prefix="",
     ckpt_dir="",
@@ -221,16 +327,20 @@ def fit_normal(
     if ckpt_iter_freq is not None:
         os.makedirs(ckpt_dir, exist_ok=True)
 
-    results = []
-    for _ in tqdm(range(n_tests), f"{opter_cls.__name__} - tests"):
-        target = target_cls(training=False)
+    metrics = {m: [] for m in ["train_loss", "train_acc", "test_loss", "test_acc"]}
+    for _ in range(n_tests):
+        train_data = data_cls(training=False)
+        test_data = data_cls(training=False)
         optee = w(optee_cls(**optee_config if optee_config is not None else {}))
         opter = opter_cls(optee.parameters(), **kwargs)
-        loss_history = []
+        for k in metrics.keys():  # new test run
+            metrics[k].append([])
         for iter_i in range(1, n_iters + 1):
-            loss = optee(target)
+            ### train
+            optee.train()
             opter.zero_grad()
-            loss.backward()
+            train_loss, train_acc = optee(train_data, return_acc=True)
+            train_loss.backward()
 
             ### model checkpointing
             if ckpt_iter_freq and iter_i % ckpt_iter_freq == 0:
@@ -240,15 +350,30 @@ def fit_normal(
                         k: v.grad for k, v in optee.all_named_parameters()
                     },
                     "optimizer": opter.state_dict(),
-                    "loss_history": loss_history,
+                    "metrics": metrics,
                 }
                 torch.save(ckpt, os.path.join(ckpt_dir, f"{ckpt_prefix}{iter_i}.pt"))
 
             ### update (after checkpointing)
             opter.step()
-            loss_history.append(loss.data.cpu().numpy())
-        results.append(loss_history)
-    return results
+
+            ### log
+            metrics["train_loss"][-1].append(train_loss.item())
+            metrics["train_acc"][-1].append(train_acc.item())
+
+            ### eval
+            if eval_iter_freq and iter_i % eval_iter_freq == 0:
+                optee.eval()
+                test_loss, test_acc = optee(test_data, return_acc=True)
+                metrics["test_loss"][-1].append(test_loss.item())
+                metrics["test_acc"][-1].append(test_acc.item())
+                optee.train()
+
+    ### metrics to numpy arrays
+    for k, v in metrics.items():
+        metrics[k] = np.array(v)
+
+    return metrics
 
 
 # def find_best_lr_normal(target_cls, optee_cls, opter_cls, **extra_kwargs):
