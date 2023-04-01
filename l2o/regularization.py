d021bd55bec7bb83553996e890773520ec5536fb
Jan Sobotka <jsobotka1188@gmail.com>
2023-04-01 12:46:32 +0200

    add funcs for regularization of conservation law breaking

--- l2o/regularization.py
+++ l2o/regularization.py
@@ -50,3 +50,72 @@ def regularize_updates_constraints(
 def regularize_update_norms(updates, optee, lr=1.0):
     """Regularizes the norm of the updates."""
     return sum([torch.norm(lr * u) for u in updates.values()])
+
+
+def regularize_translation_conservation_law_breaking(optee, params_t0):
+    """
+    The following should hold when trained under the gradient flow:
+        inner(params_t, ind_vector) = inner(params_t0, ind_vector)
+    This function regularizes the deviation from keeping this equality.
+    """
+    if (
+        params_t0["layers.final_mat.weight"].requires_grad
+        or params_t0["layers.final_mat.bias"].requires_grad
+    ):
+        print(
+            "[WARNING] Regularizing translation law breaking with initial parameters having .requires_grad = True"
+        )
+    weight_dev = torch.abs(
+        torch.sum(params_t0["layers.final_mat.weight"])
+        - torch.sum(optee.layers.final_mat.weight)
+    )
+    bias_dev = torch.abs(
+        torch.sum(params_t0["layers.final_mat.bias"])
+        - torch.sum(optee.layers.final_mat.bias)
+    )
+    return weight_dev + bias_dev
+
+
+def regularize_rescale_conservation_law_breaking(optee, params_t0):
+    """
+    The following should hold when trained under the gradient flow:
+        norm(params_t.subset_A)^2 - norm(params_t.subset_B)^2 = norm(params_t0.subset_A)^2 - norm(params_t0.subset_B)^2
+    This function regularizes the deviation from keeping this equality.
+    """
+    t0_diff = (
+        torch.norm(params_t0["layers.mat_0.weight"]) ** 2
+        + torch.norm(params_t0["layers.mat_0.bias"]) ** 2
+        - torch.norm(params_t0["layers.final_mat.weight"]) ** 2
+    )
+    t_diff = (
+        torch.norm(optee.layers.mat_0.weight) ** 2
+        + torch.norm(optee.layers.mat_0.bias) ** 2
+        - torch.norm(optee.layers.final_mat.weight) ** 2
+    )
+    return torch.abs(t0_diff - t_diff)
+
+
+def regularize_scale_conservation_law_breaking(optee, params_t0):
+    """
+    The following should hold when trained under the gradient flow:
+        norm(params_t)^2 = norm(params_t0)^2
+    This function regularizes the deviation from keeping this equality.
+    """
+    return torch.abs(
+        torch.norm(params_t0["layers.mat_0.weight"]) ** 2
+        + torch.norm(params_t0["layers.mat_0.bias"]) ** 2
+        - torch.norm(optee.layers.mat_0.weight) ** 2
+        - torch.norm(optee.layers.mat_0.bias) ** 2
+    )
+
+
+def regularize_conservation_law_breaking(
+    optee, params_t0, rescale_mul=1 / 3, scale_mul=1 / 3, translation_mul=1 / 3
+):
+    """Combines all conservation law breaking regularization functions."""
+    return (
+        regularize_rescale_conservation_law_breaking(optee, params_t0) * rescale_mul
+        + regularize_scale_conservation_law_breaking(optee, params_t0) * scale_mul
+        + regularize_translation_conservation_law_breaking(optee, params_t0)
+        * translation_mul
+    )
