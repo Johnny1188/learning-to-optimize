0cd4ffaa7711be2bcfea4c3f5dd12a49c7f2153b
Jan Sobotka <jsobotka1188@gmail.com>
2023-08-20 12:29:07 +0200

    add FashionMNIST data cls

--- l2o/data.py
+++ l2o/data.py
@@ -100,6 +100,91 @@ class MNIST:
         return batch
 
 
+class FashionMNIST:
+    def __init__(
+        self,
+        training=True,
+        batch_size=128,
+        only_classes=None,
+        preload=False,
+        normalize=False,
+        seed=DEFAULT_SEED,
+    ):
+        ### set seed
+        g = torch.Generator()
+        g.manual_seed(seed)
+
+        ### init dataset and loader
+        trans = [transforms.ToTensor()]
+        if normalize:
+            trans.append(transforms.Normalize((0.1307,), (0.3081,)))
+        if training:
+            dataset = datasets.FashionMNIST(
+                DATA_PATH,
+                train=True,
+                download=False,
+                transform=transforms.Compose(trans),
+            )
+        else:
+            dataset = datasets.FashionMNIST(
+                DATA_PATH,
+                train=False,
+                download=False,
+                transform=transforms.Compose(trans),
+            )
+
+        if only_classes is not None:
+            idx = torch.isin(
+                dataset.targets,
+                only_classes
+                if type(only_classes) == torch.Tensor
+                else torch.tensor(only_classes),
+            )
+            dataset.targets = dataset.targets[idx]
+            dataset.data = dataset.data[idx]
+
+        self.training = training
+        self.only_classes = only_classes
+        self.batch_size = batch_size
+        self.seed = seed
+
+        self.loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            worker_init_fn=seed_worker,
+            generator=g,
+        )
+
+        self.batches = []
+        self.curr_batch = 0
+
+        if preload:
+            self.preload_batches()
+
+    def count_class_samples(self):
+        """Counts the number of samples for each class in the dataset, returns a dictionary."""
+        counts = {i: 0 for i in range(10)}
+        for batch in self.loader:
+            for label in batch[1]:
+                counts[label.item()] += 1
+        return counts
+
+    def preload_batches(self, clear_batches=True):
+        if clear_batches:
+            self.batches = []
+            self.curr_batch = 0
+        for b in self.loader:
+            self.batches.append(b)
+
+    def sample(self):
+        if self.curr_batch >= len(self.batches):
+            self.preload_batches(clear_batches=True)
+        batch = self.batches[self.curr_batch]
+        self.curr_batch += 1
+        return batch
+
+
 class CIFAR10:
     def __init__(
         self,
